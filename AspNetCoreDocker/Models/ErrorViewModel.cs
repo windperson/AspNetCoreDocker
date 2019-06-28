6730ede2ba3a3a8a43e77cac85c71f7fee87e8df
Chen Yu Pao <windperson@gmail.com>
2019-06-28 16:15:51 +0800

    Add project files.

---
+++ AspNetCoreDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
